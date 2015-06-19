12e032a8cf06a63c4d5dc646605b6b4f260e0bde
Trogba Solume <toba@outlook.com>
2015-06-19 15:48:09 -0700

    UDACITY Project 1 - After INitial Review_001t

--- media.py
+++ media.py
@@ -1,11 +1,22 @@
 import webbrowser
+# Module for Displaying HTML File in Browser
 
 class  Movie():
+
+
 	def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+		"""
+			Initialize self(Local Variable) with the provided paramaters.
+		"""
 		self.title = movie_title
 		self.storyline = movie_storyline
 		self.poster_image_url = poster_image
 		self.trailer_youtube_url = trailer_youtube
 
+
 	def show_trailer(self):
+		# Open up browser with the url of the movie trailer using the Webbrowser module.
 		webbrowser.open(self.trailer_youtube_url)
+
+
+#End of Movie Class
